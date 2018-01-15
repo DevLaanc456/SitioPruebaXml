d9ad3144b8bce66b01cbca1f6c558fa5d67c3d24
Isaac Perez <isaacperez149@hotmail.com>
2018-01-14 23:34:16 -0500

    Armando el XML y la coneccion XHTTP

--- Prueba1.js
+++ Prueba1.js
@@ -1,4 +1,23 @@
+
+
 var xhttp = new XMLHttpRequest();
 
-xhttp.open('GET', 'XML/Prueba.xml', true);
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        ArmarMenu(this);
+    }
+};
+
+xhttp.open('GET', 'XML/Enlaces.xml', true);
 xhttp.send();
+
+function ArmarMenu(xml)
+{
+     var _xmldatos,_datos;
+     _xmldatos = xml.responseXML;
+     _datos = _xmldatos.getElementsByTagName("Link");
+     _cantidaddatos = _datos.length;
+      
+     alert(_cantidaddatos);
+
+}
